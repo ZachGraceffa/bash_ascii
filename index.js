f5ee78f57a91118006114393e85d4e88892302f4
Zach Graceffa <zachgraceffa@gmail.com>
2019-03-05 23:17:17 -0600

    Added cheerio.js to parse html file with ascii art

---
+++ index.js
@@ -0,0 +1,22 @@
+const libCD = require('./lib/createDocument.js');
+const fs = require('fs');
+
+if(process.argv[2] === undefined) {
+    //todo help section
+    console.log("Must pass in arguments, like -c to create from file.");
+} else if(process.argv[2] === '-c') {
+
+    if(process.argv[3] === undefined) {
+        console.log("No file specified");
+        return;
+    }
+
+    console.log("Creating bash script");
+
+    var document = libCD.createDocument(process.argv[3]);
+
+    console.log(document);
+}
+else {
+    console.log("Unrecognized input.");
+}
